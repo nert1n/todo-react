182cd8cd4ded4fbe364b6fb382e7dfbfe2f2dd0f
nert1n <dymufa@gmail.com>
2025-01-27 19:07:28 +0200

    feat: deleted unused holders and code

--- src/shared/lib/hooks/use-convert-to-time.ts
+++
@@ -1,8 +0,0 @@
-const useConvertToTime = (milliseconds: number) => {
-	const minutes = Math.floor(milliseconds / 60000);
-	const seconds = ((milliseconds % 60000) / 1000).toFixed(0);
-	const time = minutes + ":" + (Number(seconds) < 10 ? "0" : "") + seconds;
-	return { time };
-};
-
-export default useConvertToTime;
